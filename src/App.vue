4d0cd2adb495019448a04fd0a2d47352251a7375
Andsirpa <andsirpa94@gmail.com>
2024-02-13 16:19:28 +0100

    feat: added AppHero

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <script>
 import AppTitle from './components/AppTitle.vue';
+import AppHero from './components/AppHero.vue';
 
 export default {
   data() {
@@ -8,13 +9,14 @@ export default {
     };
   },
 
-  components: { AppTitle },
+  components: { AppTitle, AppHero },
 };
 </script>
 
 <template>
   <header>
     <app-title />
+    <app-hero />
   </header>
 </template>
 
---
+++ src/components/AppHero.vue
@@ -0,0 +1,27 @@
+<script>
+export default {
+    data() {
+        return {
+            title: 'Ciao!',
+            text: 'Questa è la mia app!'
+        };
+    },
+};
+</script>
+
+<template>
+    <div class="jumbotron">
+        <h1>{{ title }}</h1>
+        <p>{{ text }}</p>
+
+    </div>
+</template>
+
+<style>
+.jumbotron {
+    background-color: #ccc;
+    padding: 20px;
+    text-align: center;
+    border-radius: 5px;
+}
+</style>
